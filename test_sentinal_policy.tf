f909d7d4c2ad05fe1fdbaf39b86b8f24bf429f9f
ctalaveraw <ctalaveraw@gmail.com>
2021-05-31 16:22:47 -0400

    created a test sentinal policy file

---
+++ test_sentinal_policy.tf
@@ -0,0 +1,12 @@
+# sentinal is enforcing that any created
+# EC2 instance must have at least '1' tag applied
+
+import "tfplan"
+
+main = rule {
+    all tfplan.resources.aws_instance as _, instances {
+        all instances as _, r {
+            (length(r.applied.tags) else 0) > 0
+        }
+    }
+}
