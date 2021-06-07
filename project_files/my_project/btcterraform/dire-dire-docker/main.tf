7f3af629089393a94f8c72b60e24008c6d50b736
ctalaveraw <ctalaveraw@gmail.com>
2021-06-07 19:03:56 -0400

    updated with 'Terraform State File Deep Dive' and 'Terraform Console Outputs'

--- project_files/my_project/btcterraform/dire-dire-docker/main.tf
+++ project_files/my_project/btcterraform/dire-dire-docker/main.tf
@@ -1,40 +1,3 @@
-## when executing a `terraform apply` command
-## all files with a .tf extention will be read.
-## the first one read will be "main.tf"
-
-## this project will be called "dire-dire-docker"
-## (Super Mario!) and will be dealing with Terraform
-## and Docker. This file will be the main.tf file.
-
-## the first block needs to be defined, the "terraform: block"
-## this will specify what providers will be used. in this project
-## Docker is the provider
-
-## next, the `provider` block needs to specify information about
-## Docker and it's configuration
-
-## terraform {
-##    required_providers {
-##        docker = {
-##            source = "terraform-providers/docker"
-##        }
-##    }
-## }
-##
-## provider "docker" {}
-
-## for this example, just an empty set of brackets will pull
-## the latest provider info from the public docker repo.
-## at this point, the file can be saved and `terraform init` can 
-## be run to use Terraform in this working directory
-## best practice is to specify version so updates don't break code
-
-
-
-## for the real code, the provider will be specified.
-## the version number has to be between 2.7.2 and 2.7.9
-
-
 terraform {
   required_providers {
     docker = {
@@ -57,4 +20,14 @@ resource "docker_container" "ddd_nodeRED_container_1" {
     internal = 1880
     external = 1880
   }
+}
+
+output "IPv4_Address" { # this is an output variable to grab the container's IP
+  value = docker_container.ddd_nodeRED_container_1.ip_address
+  description = "The IPv4 address assigned to the running container"
+}
+
+output "container_name" { # this is an output variable to grab the container's assigned name
+  value = docker_container.ddd_nodeRED_container_1.name
+  description = "The assigned name of the created container"
 }
