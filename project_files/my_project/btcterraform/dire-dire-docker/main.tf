d96feffbfe60cab2a045d38f637b4d2bf33810e6
ctalaveraw <ctalaveraw@gmail.com>
2021-06-09 15:16:23 -0400

    pushing todays work on using `count` statements with referring indexes

--- project_files/my_project/btcterraform/dire-dire-docker/main.tf
+++ project_files/my_project/btcterraform/dire-dire-docker/main.tf
@@ -14,7 +14,8 @@ resource "docker_image" "nodered_image_base" {
 }
 
 resource "docker_container" "ddd_nodeRED_container_1" {
-  name  = join("-", ["nodeRED", random_string.random_container_name.result])
+  count = 2
+  name  = join("-", ["nodeRED", random_string.random_container_name[count.index].result])
   image = docker_image.nodered_image_base.latest
   ports {
     internal = 1880
@@ -22,17 +23,28 @@ resource "docker_container" "ddd_nodeRED_container_1" {
   }
 }
 
-output "container_socket" { # this is an output variable to grab the container's IP
-  value = join(":", [docker_container.ddd_nodeRED_container_1.ip_address, docker_container.ddd_nodeRED_container_1.ports[0].external])
+output "container_0_socket" { # this is an output variable to grab the container's IP
+  value = join(":", [docker_container.ddd_nodeRED_container_1[0].ip_address, docker_container.ddd_nodeRED_container_1[0].ports[0].external])
   description = "The IPv4 address assigned to the running container"
 }
 
-output "container_name" { # this is an output variable to grab the container's assigned name
-  value = docker_container.ddd_nodeRED_container_1.name
+output "container_1_socket" { # this is an output variable to grab the container's IP
+  value = join(":", [docker_container.ddd_nodeRED_container_1[1].ip_address, docker_container.ddd_nodeRED_container_1[1].ports[0].external])
+  description = "The IPv4 address assigned to the running container"
+}
+
+output "container_0_name" { # this is an output variable to grab the container's assigned name
+  value = docker_container.ddd_nodeRED_container_1[0].name
+  description = "The assigned name of the created container"
+}
+
+output "container_1_name" { # this is an output variable to grab the container's assigned name
+  value = docker_container.ddd_nodeRED_container_1[1].name
   description = "The assigned name of the created container"
 }
 
 resource "random_string" "random_container_name" {
+  count = 2
   length = 4
   special = false
   upper = false
