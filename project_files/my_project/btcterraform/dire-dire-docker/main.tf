98f597416e8d92682f5420373391539253b78a45
ctalaveraw <ctalaveraw@gmail.com>
2021-06-10 19:17:41 -0400

    added notes on splat, for loops, tainting and state-locking

--- project_files/my_project/btcterraform/dire-dire-docker/main.tf
+++ project_files/my_project/btcterraform/dire-dire-docker/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
+      source = "kreuzwerker/docker"
     }
   }
 }
@@ -12,39 +12,30 @@ resource "docker_image" "nodered_image_base" {
   name = "nodered/node-red:latest"
 }
 
+
 resource "docker_container" "ddd_nodeRED_container_1" {
   count = 2
   name  = join("-", ["nodeRED", random_string.random_container_name[count.index].result])
   image = docker_image.nodered_image_base.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880 # duplicate external ports on host not supported, so cannot be manually set
   }
 }
 
-output "container_0_socket" { # this is an output variable to grab the container's IP
-  value = join(":", [docker_container.ddd_nodeRED_container_1[0].ip_address, docker_container.ddd_nodeRED_container_1[0].ports[0].external])
-  description = "The IPv4 address assigned to the running container"
-}
-
-output "container_1_socket" { # this is an output variable to grab the container's IP
-  value = join(":", [docker_container.ddd_nodeRED_container_1[1].ip_address, docker_container.ddd_nodeRED_container_1[1].ports[0].external])
-  description = "The IPv4 address assigned to the running container"
+resource "random_string" "random_container_name" {
+  count   = 2
+  length  = 4
+  special = false
+  upper   = false
 }
 
 output "container_name" { # this is an output variable to grab the container's assigned name
-  value = docker_container.ddd_nodeRED_container_1[*].name
+  value       = docker_container.ddd_nodeRED_container_1[*].name
   description = "The assigned name of the created container"
 }
 
-# output "container_1_name" { # this is an output variable to grab the container's assigned name
-#   value = docker_container.ddd_nodeRED_container_1[*].name
-#   description = "The assigned name of the created container"
-# }
-
-resource "random_string" "random_container_name" {
-  count = 2
-  length = 4
-  special = false
-  upper = false
+output "container_socket" { # this is an output variable to grab the container's IP
+  value       = [for created_loop_1 in docker_container.ddd_nodeRED_container_1[*] : join(":", [created_loop_1.ip_address], created_loop_1.ports[*]["external"])]
+  description = "The IPv4 address assigned to the running container, and external port"
 }
