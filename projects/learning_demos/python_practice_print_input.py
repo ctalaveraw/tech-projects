3e057cd089c9263df00dfa9444d07963eccde57f
ctalaveraw <ctalaveraw@gmail.com>
2021-10-09 17:18:24 -0400

    adding python practice program, using input and print functions

---
+++ projects/learning_demos/python_practice_print_input.py
@@ -0,0 +1,5 @@
+## New DevOps need to get good at Python, so this is the way to go..
+print(f"Hey newbie DevOps guy! \nLet's practice Python")
+input("Tap any key to continue!\n")
+print(f"This program is practicing the 'input' and 'print' functions")
+## 
