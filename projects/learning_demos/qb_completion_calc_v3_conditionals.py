f9f240b16fba77b77025353f7e6e1b5dcbc78843
ctalaveraw <ctalaveraw@gmail.com>
2021-10-20 18:54:11 -0400

    fixed bugs with v3 of program

--- projects/learning_demos/qb_completion_calc_v3_conditionals.py
+++ projects/learning_demos/qb_completion_calc_v3_conditionals.py
@@ -1,7 +1,7 @@
 ## Program to calculate completion percentage of a quarterback
 ## Stitched together by: gtihub.com/ctalaveraw
 
-## v3 implements conditionals.
+## v3 implements conditionals, Booleans and nested if/else statements.
 ## This is for input validation so only values that make sense can be provided.
 
 
@@ -12,18 +12,18 @@ def continue_message():
 ## A function for displaying error messages.
 def error_message():
     message = print('Invalid value!\nOnly whole numbers greater than 0 allowed!')
-    print(message)
+    return(message)
 
 ## This block introduces the user to the program.
 print('\nHello user!\n')
 print('\n\nWelcome to this hastily-made QB completion percentage calculator')
 print('Release v3.0 - using conditionals and booleans for input validation.')
-print('I hope you like American football...\n')
-continue_message()
-print('The code for the function will be made in the background.\n')
+print('I hope you like American football!\n')
 continue_message()
 
-
+## Now two seperate input statements, stored in variables, will be created to store the data
+input_attempts = input('Please enter the total number of attempted passes:\n')
+input_complete = input('Please enter the total number of completed passes:\n')
 
 
 ## A function will be created, with two parameters representing the input data to be passed along.
@@ -31,21 +31,21 @@ def calc_completion_percent(completion, attempt):
         completion_percentage = float((completion / attempt) * 100)
         return(f'Out of {attempt} balls, {completion} were caught, completing {completion_percentage}% of throws.')
 
+
 ## This will be a conditional statement in a custom function to be sure input is validated.
-## the built in ".isdigit()" function checks wheter an integer value is used.
+## The built in ".isdigit()" function checks wheter positive integer value is used.
 def validate_calc_completion_percent():
-    if input_attempts.isdigit() and input_complete.isdigit():
-        return_completion_percent = calc_completion_percent(int(input_complete), int(input_attempts))
-        if input_complete == 0 or input_attempts == 0:
-            error_message()
-        print(return_completion_percent)
-    else:
+    if input_attempts.isdigit() and input_complete.isdigit(): #both inputs have to be positive digits
+        if (int(input_attempts) > 0) and (int(input_complete) > 0): #executing if both inputs greater than 0
+            return_completion_percent = calc_completion_percent(int(input_complete), int(input_attempts))
+            print(return_completion_percent)
+        elif (int(input_attempts) == 0) or (int(input_complete) == 0): #catching any zero values
+            print('All values must be greater than 0!\n') 
+    else: #catching any non positive integers
         error_message()
 
-## Now two seperate input statements, stored in variables, will be created to store the data
-input_attempts = input("Please enter the total number of attempted passes:\n")
-input_complete = input("Please enter the total number of completed passes:\n")
 
+## Calling this function runs the program once.
 validate_calc_completion_percent()
 
 
@@ -53,4 +53,4 @@ validate_calc_completion_percent()
 ## End of program.
 input('Press any key to exit.\n')
 
-## FIX DIVIDE BY ZERO ERROR
+## Issues with this program, is that out of bounds values can be entered
