27806d3dfa745d4bc0ece4d51a0a4beec056088d
ctalaveraw <ctalaveraw@gmail.com>
2021-11-05 16:45:02 -0400

    cleared preexisting files from java branch

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# **Python** project branch
+# **Java** project branch
 
 
-This branch contains Python projects
+This branch contains Java projects.
+
+Nothing too intense but a proof-of-concept to demonstrate artifact building and management 
--- python_projects/learning_demos/10-jiffy_calc_v4_modular/venv/global_var.py
+++
@@ -1,9 +0,0 @@
-# Global variables are defined here
-
-continue_message = str('Tap any key!\n')
-terminate_program = str('exit')
-
-
-# A function representing the continue message will be created.
-def continue_program():
-    input(continue_message)
