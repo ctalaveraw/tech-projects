32d463b9e2b20bd2a71b84c1c9eabb3cacf9e874
ctalaveraw <ctalaveraw@gmail.com>
2021-11-14 22:53:09 -0500

    test change

--- java_projects/hello_world_app/DisplayMessage.java
+++ java_projects/hello_world_app/DisplayMessage.java
@@ -2,6 +2,7 @@
 // This is just a demonstration of a simple Java app, which I want to package and store 
 // in an artifact repo. 
 // Trying to challenge myself :)
+//
 
 public class DisplayMessage { // I named this file "DisplayMessage' to get this program to run
   public static void main(String[] args) {
